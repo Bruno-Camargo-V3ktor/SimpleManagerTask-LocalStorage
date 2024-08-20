f7347eb5c5e663444db32d7f6b32d42f84e06ce2
Bruno C. <v3ktoryt@gmail.com>
2024-08-19 21:20:26 -0300

    Removendo o Modal e refatorando os CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
 import './styles/App.css'
-//import commingSoon from './asserts/Animated Rotating Coming Soon.gif'
+
 
 import Card from './components/Card';
 import TodoList from './components/TodoList';
-import Modal from './components/Modal';
 
 function App() {
   return (
     <div className="App">
 
-      <Modal />
-
-      {/* <img 
-        src={commingSoon} 
-        style={ {width: '400px', height:'400px', position:'absolute', right: '25px', bottom: '25px'} } 
-      /> */}
-
       <main>
 
         <Card showTitle={false} >
-
           <TodoList />
-
         </Card>
 
       </main>
